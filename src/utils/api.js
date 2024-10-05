a6432102b5f441786787ad8acc3a8b4aa5b4c9c0
atulchougale <atulchougale96@gmail.com>
2024-10-06 05:01:18 +0530

    red

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'|| 'https://socialmediaapp-4gwv.onrender.com',
+  baseURL: 'https://socialmediaapp-4gwv.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
