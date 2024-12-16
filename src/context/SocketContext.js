914341575b12780c04e4198b706fc9b8e6b454de
atulchougale <atulchougale96@gmail.com>
2024-12-16 23:38:20 +0530

    api

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
   const { authUser } = useAuth();
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000/", {
+      const socket = io("https://socialmediaapp-4gwv.onrender.com/", {
         query: {
           userId: authUser?._id,
         },
